d78c9ffaf314d50e330755d3095a315e0ee8348e
Mohamed Bengrich <mohamed@etamin.studio>
2023-12-18 10:02:18 +0100

    add proprety type

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,7 +3,7 @@ class Property < ApplicationRecord
     accepts_nested_attributes_for :property_photos, allow_destroy: true
 
     enum status: { vente: 0, location: 1 }
-    enum property_type: { maison: 0, appartement: 1 }
+    enum property_type: { maison: 0, appartement: 1, maisons_de_ville: 3, penthouses: 4,  plots: 5}
   
     validates :title, :description, :price, :address, :bedrooms, :bathrooms, :area, :property_type, :status, presence: true    
 end
--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     bedrooms { Faker::Number.between(from: 1, to: 5) }
     bathrooms { Faker::Number.between(from: 1, to: 3) }
     area { Faker::Number.between(from: 20, to: 200) }
-    property_type { ["maison", "appartement"].sample }
+    property_type { ["maison", "appartement", "maisons_de_ville", "penthouses", "plots"].sample }
     status { ["vente", "location"].sample }
 
     after(:build) do |property|
